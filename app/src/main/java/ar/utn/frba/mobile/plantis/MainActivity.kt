7de1dca2d41924720b1590e983e8fce2424bc46e
dpeccia <diegoignaciopeccia@gmail.com>
2022-05-02 22:56:53 -0300

    navigation now works between two fragments

--- app/src/main/java/ar/utn/frba/mobile/plantis/MainActivity.kt
+++ app/src/main/java/ar/utn/frba/mobile/plantis/MainActivity.kt
@@ -2,10 +2,14 @@ package ar.utn.frba.mobile.plantis
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import ar.utn.frba.mobile.plantis.databinding.MainActivityBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
     lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,6 +17,12 @@ class MainActivity : AppCompatActivity() {
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
         setSupportActionBar(binding.toolbar)
-        binding.navigationbar.itemIconTintList = null
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val navView = binding.navigationbar
+        navView.itemIconTintList = null
+        navView.setupWithNavController(navController)
     }
 }
