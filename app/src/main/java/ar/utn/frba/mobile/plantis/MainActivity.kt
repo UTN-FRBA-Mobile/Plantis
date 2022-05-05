d0f65ee8978c12bd0d1f4f47df60b213dad328a7
Diego Peccia <42644816+dpeccia@users.noreply.github.com>
2022-05-05 20:44:34 -0300

    Navigation (#20)
    * WIP: bottom nav bar
    
    * adding myGarden to the bottom nav bar
    
    * navigation now works between two fragments
    
    * adding search icon to the bottom nav bar
    
    * adding reminders and drplantis to the bottom nav bar
    
    * increasing icon size
    
    * adding file to gitignore
    
    * deleting icon background

--- app/src/main/java/ar/utn/frba/mobile/plantis/MainActivity.kt
+++ app/src/main/java/ar/utn/frba/mobile/plantis/MainActivity.kt
@@ -1,11 +1,28 @@
 package ar.utn.frba.mobile.plantis
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
+import ar.utn.frba.mobile.plantis.databinding.MainActivityBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val navView = binding.navigationbar
+        navView.itemIconTintList = null
+        navView.setupWithNavController(navController)
     }
 }
