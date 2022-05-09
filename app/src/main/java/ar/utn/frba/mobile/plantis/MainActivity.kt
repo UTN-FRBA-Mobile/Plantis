ade8bf40373ca77254674774a703cb1db8f379a9
dpeccia <diegoignaciopeccia@gmail.com>
2022-05-09 11:07:27 -0300

    little fix

--- app/src/main/java/ar/utn/frba/mobile/plantis/MainActivity.kt
+++ app/src/main/java/ar/utn/frba/mobile/plantis/MainActivity.kt
@@ -1,7 +1,10 @@
 package ar.utn.frba.mobile.plantis
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import ar.utn.frba.mobile.plantis.databinding.MainActivityBinding
@@ -11,28 +14,24 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding: MainActivityBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        try {
-            setTheme(R.style.AppTheme)
-            super.onCreate(savedInstanceState)
-            binding = MainActivityBinding.inflate(layoutInflater)
-            setContentView(binding.root)
-            setSupportActionBar(binding.toolbar)
+        setTheme(R.style.AppTheme)
+        super.onCreate(savedInstanceState)
+        binding = MainActivityBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
-            val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-            val navController = navHostFragment.navController
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
 
-            val navView = binding.navigationbar
-            navView.itemIconTintList = null
-            navView.setupWithNavController(navController)
-        } catch (e: Exception) {
-            print(e.message)
-        }
+        val navView = binding.navigationbar
+        navView.itemIconTintList = null
+        navView.setupWithNavController(navController)
     }
 
-    /*fun getUrlFromIntent(view: View) {
+    fun getUrlFromIntent(view: View) {
         val url = "https://simple.wikipedia.org/wiki/Chlorophytum_comosum"
         val intent = Intent(Intent.ACTION_VIEW)
         intent.data = Uri.parse(url)
         startActivity(intent)
-    }*/
+    }
 }
