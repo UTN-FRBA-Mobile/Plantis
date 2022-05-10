9213b8827a7e1b523cc8db520f31a0a621f65f3a
Francisco Cortez <37709437+FranciscoII@users.noreply.github.com>
2022-05-10 09:47:31 -0300

    Search screen (#26)
    * WIP Go to camera when press button
    
    * Get a image from the camera to the app
    
    * Quito prints
    
    * Agrego navegacion
    
    * Extraigo strings a resources
    
    * fix commnents and warnings

---
+++ app/src/main/java/ar/utn/frba/mobile/plantis/fragments/SearchResultsFragment.kt
@@ -0,0 +1,18 @@
+package ar.utn.frba.mobile.plantis.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import ar.utn.frba.mobile.plantis.R
+
+class SearchResultsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_search_results, container, false)
+    }
+}
