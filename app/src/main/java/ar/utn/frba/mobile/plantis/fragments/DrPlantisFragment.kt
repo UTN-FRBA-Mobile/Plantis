ad954a88337b7407fc27995f66a052e59b58e9fe
Diego Peccia <42644816+dpeccia@users.noreply.github.com>
2022-05-16 10:47:24 -0300

    Camera handling improvement (#33)

--- app/src/main/java/ar/utn/frba/mobile/plantis/fragments/DrPlantisFragment.kt
+++ app/src/main/java/ar/utn/frba/mobile/plantis/fragments/DrPlantisFragment.kt
@@ -1,53 +1,31 @@
 package ar.utn.frba.mobile.plantis.fragments
 
-import android.app.Activity
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import ar.utn.frba.mobile.plantis.CameraHandler
 import ar.utn.frba.mobile.plantis.R
 import ar.utn.frba.mobile.plantis.databinding.FragmentDrPlantisBinding
-import ar.utn.frba.mobile.plantis.databinding.FragmentSearchBinding
 
 class DrPlantisFragment : Fragment() {
-
     lateinit var binding: FragmentDrPlantisBinding
-    val REQUEST_IMAGE_CAPTURE = 1888
-
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentDrPlantisBinding.inflate(inflater, container, false)
-        binding.drplantisSearchButton.setOnClickListener{
-            dispatchTakePictureIntent()
-        }
         return binding.root
     }
 
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.drplantisNextButton.setOnClickListener { //TODO
-           val action = R.id.action_drPlantisFragment_to_drPlantisSearchResultsFragment
-           findNavController().navigate(action)
-        }
-    }
+        val cameraHandler = CameraHandler(binding.drplantisSearchImageView, binding.drplantisNextButton, ::registerForActivityResult)
 
-
-    @Suppress("DEPRECATION")
-    private fun dispatchTakePictureIntent() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE)
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.drplantisSearchImageView.setImageBitmap(imageBitmap)
-            binding.drplantisNextButton.setVisibility(View.VISIBLE)
+        binding.drplantisSearchButton.setOnClickListener { cameraHandler.launchTakePictureIntent() }
+        binding.drplantisNextButton.setOnClickListener {
+            val action = R.id.action_fragment_search_to_fragment_search_results
+            findNavController().navigate(action)
         }
     }
 }
--- app/src/main/java/ar/utn/frba/mobile/plantis/fragments/SearchFragment.kt
+++ app/src/main/java/ar/utn/frba/mobile/plantis/fragments/SearchFragment.kt
@@ -1,53 +1,31 @@
 package ar.utn.frba.mobile.plantis.fragments
 
-import android.app.Activity.RESULT_OK
-import android.content.Intent
-import android.graphics.Bitmap
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
-import android.widget.ImageView
 import androidx.fragment.app.Fragment
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.findNavController
+import ar.utn.frba.mobile.plantis.CameraHandler
 import ar.utn.frba.mobile.plantis.R
 import ar.utn.frba.mobile.plantis.databinding.FragmentSearchBinding
 
-
 class SearchFragment : Fragment() {
     lateinit var binding: FragmentSearchBinding
-    val REQUEST_IMAGE_CAPTURE = 1888
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentSearchBinding.inflate(inflater, container, false)
-        binding.searchButton.setOnClickListener{
-            dispatchTakePictureIntent()
-        }
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val cameraHandler = CameraHandler(binding.searchImageView, binding.nextButton, ::registerForActivityResult)
+
+        binding.searchButton.setOnClickListener { cameraHandler.launchTakePictureIntent() }
         binding.nextButton.setOnClickListener {
             val action = R.id.action_fragment_search_to_fragment_search_results
             findNavController().navigate(action)
-        }
-    }
-
-
-    @Suppress("DEPRECATION")
-    private fun dispatchTakePictureIntent() {
-        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-        startActivityForResult(cameraIntent, REQUEST_IMAGE_CAPTURE)
-    }
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-            val imageBitmap = data?.extras?.get("data") as Bitmap
-            binding.searchImageView.setImageBitmap(imageBitmap)
-            binding.nextButton.setVisibility(View.VISIBLE)
         }
     }
 }
