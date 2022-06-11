ba4d6117e6067ca6fa355c3c47fac542e46797fe
Magali Modugno <magalimodugno@gmail.com>
2022-06-11 18:23:44 -0300

    wip notifications

--- app/src/main/java/ar/utn/frba/mobile/plantis/MainActivity.kt
+++ app/src/main/java/ar/utn/frba/mobile/plantis/MainActivity.kt
@@ -1,14 +1,22 @@
 package ar.utn.frba.mobile.plantis
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import ar.utn.frba.mobile.plantis.databinding.MainActivityBinding
 
+const val CHANNEL_ID = "1"
 class MainActivity : AppCompatActivity() {
     lateinit var binding: MainActivityBinding
 
@@ -26,10 +34,50 @@ class MainActivity : AppCompatActivity() {
         navView.itemIconTintList = null
         navView.setupWithNavController(navController)
 
+
+
+
     }
 
 
     fun getUrlFromIntent(view: View) {
         startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(Global.instance.data)))
     }
+
+    fun notificate(){
+
+        val intent = Intent(this, AlertDetails::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+        val builder = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setSmallIcon(R.drawable.splash_screen)
+            .setContentTitle("My notification")
+            .setContentText("Hello World!")
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            // Set the intent that will fire when the user taps the notification
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true)
+        }
+    }
+
+    private fun createNotificationChannel() {
+        // Create the NotificationChannel, but only on API 26+ because
+        // the NotificationChannel class is new and not in the support library
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "name"
+            val descriptionText = "channel desc"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system
+            val notificationManager: NotificationManager =
+                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+        }
+    }
+
+
 }
