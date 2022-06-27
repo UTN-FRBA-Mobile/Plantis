e73a73ed99195fbaa062dbb04bec60b20850b32f
FranciscoII <francortez993@gmail.com>
2022-06-27 15:14:16 -0300

    new PlantDiseaseFragment and navigation

--- app/src/main/java/ar/utn/frba/mobile/plantis/DrPlantisResultsAdapter.kt
+++ app/src/main/java/ar/utn/frba/mobile/plantis/DrPlantisResultsAdapter.kt
@@ -4,12 +4,15 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.core.os.bundleOf
 import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import ar.utn.frba.mobile.plantis.client.Suggestion
+import ar.utn.frba.mobile.plantis.client.healthAssesment.Disease
+import ar.utn.frba.mobile.plantis.client.healthAssesment.DiseaseDetails
 import ar.utn.frba.mobile.plantis.client.healthAssesment.HealthAssessment
 import com.bumptech.glide.Glide
 
@@ -19,14 +22,13 @@ class DrPlantisResultsAdapter(
     class DrPlantisResultsViewHolder(val view: View, val context: Context) :
         RecyclerView.ViewHolder(view) {
         val titleText: TextView = view.findViewById(R.id.title)
-        val icon: ImageView = view.findViewById(R.id.icon)
         val subtitleText: TextView = view.findViewById(R.id.description)
-        val infoButton: ImageView = view.findViewById(R.id.info)
+        val treatmentButton: Button = view.findViewById(R.id.treatment_button)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DrPlantisResultsViewHolder {
         val context = parent.context
-        val view: View = LayoutInflater.from(context).inflate(R.layout.custom_list, parent, false)
+        val view: View = LayoutInflater.from(context).inflate(R.layout.diseases_list, parent, false)
         return DrPlantisResultsViewHolder(view, context)
     }
 
@@ -34,29 +36,17 @@ class DrPlantisResultsAdapter(
         val disease = healthAssessment.diseases?.get(position)
         val diseaseName = disease?.name
         val context = holder.context
-//
-//        Glide.with(context).load(suggestion.plantDetails?.wikiImage?.value).into(holder.icon)
+
         holder.titleText.text = diseaseName
-        holder.subtitleText.text = context.getString(R.string.Probabibilidad,"%.2f".format(disease?.probability))
-
-//        holder.infoButton.setOnClickListener {
-//            val action = R.id.action_fragment_search_results_to_myPlantisFragment
-//            val bundle = bundleOf(
-//                "details" to mapSuggestionToPlantDetail(suggestion),
-//            )
-//            Navigation.findNavController(view).navigate(action, bundle)
-//        }
-
-//    private fun mapSuggestionToPlantDetail(suggestion: Suggestion): PlantDetail {
-//        val plantDetails = suggestion.plantDetails
-//        return PlantDetail(
-//            name = plantDetails?.commonNames?.first(),
-//            scientificName = plantDetails?.scientificName,
-//            description = plantDetails?.wikiDescription?.value,
-//            imageUrl = plantDetails?.wikiImage?.value,
-//            wikiUrl = plantDetails?.url
-//        )
-//    }
+        holder.subtitleText.text =
+            context.getString(R.string.Probabibilidad, "%.2f".format(disease?.probability))
+        holder.treatmentButton.setOnClickListener {
+            val action = R.id.action_dr_plantis_search_results_to_plant_disease_fragment
+            val bundle = bundleOf(
+                "diseaseDetails" to disease?.diseaseDetails,
+            )
+            Navigation.findNavController(view).navigate(action, bundle)
+        }
     }
     override fun getItemCount() = healthAssessment.diseases!!.size
 
