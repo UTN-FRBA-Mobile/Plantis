3078b8a64d3cf58955523ce3b0c421f801bb7000
Francisco Cortez <37709437+FranciscoII@users.noreply.github.com>
2022-07-07 14:13:13 -0300

    Mostrar resultados en DrPlantisResultsFragment (#54)
    * First view health results
    
    * new PlantDiseaseFragment and navigation
    
    * new styles
    
    * quito layout innecesario

--- app/src/main/java/ar/utn/frba/mobile/plantis/fragments/DrPlantisSearchResultsFragment.kt
+++ app/src/main/java/ar/utn/frba/mobile/plantis/fragments/DrPlantisSearchResultsFragment.kt
@@ -1,25 +1,61 @@
 package ar.utn.frba.mobile.plantis.fragments
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import ar.utn.frba.mobile.plantis.DrPlantisResultsAdapter
+import ar.utn.frba.mobile.plantis.R
 import ar.utn.frba.mobile.plantis.R.layout.fragment_dr_plantis_search_results
+import ar.utn.frba.mobile.plantis.SearchResultsAdapter
 import ar.utn.frba.mobile.plantis.client.healthAssesment.HealthAssessment
+import ar.utn.frba.mobile.plantis.databinding.FragmentDrPlantisSearchResultsBinding
+import ar.utn.frba.mobile.plantis.databinding.FragmentMyPlantisBinding
 
 class DrPlantisSearchResultsFragment : Fragment() {
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(fragment_dr_plantis_search_results, container, false)
+    lateinit var binding: FragmentDrPlantisSearchResultsBinding
+    lateinit var _context: Context
+    lateinit var recyclerView: RecyclerView
+    private val fullCircularProgress: Int = 50
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        binding = FragmentDrPlantisSearchResultsBinding.inflate(inflater, container, false)
+        _context = container!!.context
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val healthAssessment = arguments?.getParcelable<HealthAssessment>("healthAssessment")
-        println(healthAssessment)
+//        binding.isHealthyProbText.text = _context.getString(R.string.health_probability,"%.2f".format(healthAssessment?.isHealthyProbability))
+        val viewManager = LinearLayoutManager(this.context)
+        val viewAdapter = DrPlantisResultsAdapter(view, healthAssessment!!)
+        val healthyProbabilitity = healthAssessment?.isHealthyProbability!!
+
+        binding.progressCircular.progress = getHealthLevel(healthyProbabilitity)
+
+        if(healthyProbabilitity >= 0.70){
+            binding.healthText.text = "Healthy"
+        }
+        if(healthyProbabilitity < 0.70){
+            binding.healthText.text = "Could be better"
+        }
+        if(healthyProbabilitity < 0.30){
+            binding.healthText.text = "Needs treatment"
+        }
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.dr_plantis_result_recycler_view).apply{
+            layoutManager = viewManager
+            adapter = viewAdapter
+        }
     }
-}
+
+    private fun getHealthLevel(percentage: Double): Int {
+        return (fullCircularProgress * percentage).toInt()
+    }
+    }
